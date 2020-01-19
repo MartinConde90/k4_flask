5bd2495f7540197eacc08bed7563b49d3967bd4a
= <=>
2020-01-19 04:32:14 +0100

    .

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,10 @@
 from myapp import app #del modulo app me importas la instancia
+from flask import render_template #render_template es una funcion
 
 @app.route("/") #este app es la instancia del __init__
 def index():
-    return 'Aquí irá mi app'
+    return render_template('index.html')
+
+@app.route("/detail")
+def detail():
+    return render_template('detail.html')
